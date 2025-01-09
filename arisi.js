a038f21549f903b3b0e221f083964cfe2724d28e
PriyaT <thangamanithangamani7775@.com>
2025-01-09 23:31:49 +0530

    added KATTABOMMAN HOTEL

---
+++ arisi.js
@@ -0,0 +1,8 @@
+<html>
+<head>
+
+</head>
+<body>
+    <h1>Arisi details</h1>
+</body>
+<</html>
---
+++ sample.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+</head>
+<body>
+    <script>
+        function disp()
+        {
+            var a;
+            a=parseInt(window.prompt("Enter the number : "));
+            
+
+        }
+        </script>
+</body>
+</html>
